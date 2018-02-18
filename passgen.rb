2d10228ef3135eefc880785fe2972485318a4cc3
Auralcat <ashleyzt@hi2.in>
2018-02-18 11:32:45 -0300

    Introduce option parsing

--- passgen.rb
+++ passgen.rb
@@ -1,18 +1,42 @@
 # CLI password generator in Ruby.
 
-require 'optionparser'
+require 'optparse'
 
+# This is the logic part of the program
 class PasswordGenerator
-  attr_accessor :length
-  ALL_CHARS = (32).upto(126).map{|n| n.chr} # full char space
+
+  # Initialize constants
+  ALL_CHARS = (32).upto(126).map{|n| n.chr} # includes parens, brackets, etc
   ALPHANUMERIC = "a".upto("z").to_a
     .concat("A".upto("Z").to_a)
     .concat("0".upto("9").to_a)
-  def generate
-    # outputs a random password with the initialized length
-    ALPHANUMERIC
+
+  def initialize(options)
+    @options = options
+  end
+
+  def generate_alpha
+
   end
 end
+
+# Treating options on input
+options = {}
+
+OptionParser.new do |parser|
+
+  parser.on("-a",
+            "--alphanumeric",
+            "Generate password only with letters and numbers") do |v|
+    options[:alpha] = v
+  end
+
+  parser.on("-l",
+            "--length LENGTH",
+            "Generate password with length LENGTH. Default is 10.") do |l|
+    options[:length] = l
+  end
+end.parse!
 
 p = PasswordGenerator.new
 p p.generate
