08aa94e54581603f764eb188c511a30809cacd35
Auralcat <ashleyzt@hi2.in>
2018-02-18 11:21:27 -0300

    Add alphanumeric letter space

--- passgen.rb
+++ passgen.rb
@@ -4,11 +4,15 @@ require 'optionparser'
 
 class PasswordGenerator
   attr_accessor :length
-  ALL_CHARS = (32).upto(126).map{|n| n.chr}
+  ALL_CHARS = (32).upto(126).map{|n| n.chr} # full char space
+  ALPHANUMERIC = "a".upto("z").to_a
+    .concat("A".upto("Z").to_a)
+    .concat("0".upto("9").to_a)
   def generate
     # outputs a random password with the initialized length
-    ALL_CHARS
+    ALPHANUMERIC
   end
 end
 
 p = PasswordGenerator.new
+p p.generate
