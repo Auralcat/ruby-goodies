8623f2d216493a4cec2b3c7c365def59839e2c10
Auralcat <ashleyzt@hi2.in>
2018-02-26 20:14:46 -0300

    Write test for emoji passwords

--- passgen/test_passgen.rb
+++ passgen/test_passgen.rb
@@ -14,4 +14,10 @@ class TestPasswordGenerator < Minitest::Test
   def test_password_has_all_chars_on_default
     assert_equal true, @passgen.generate.match(/\A[a-zA-z0-9]*\z/).nil?
   end
+
+  def test_outputs_emoji_password_with_option_e
+    emoji_passgen = PasswordGenerator.new(:length => 10, :count => 1,
+                                          :emoji => true)
+    assert_equal true, @passgen.generate.has_emoji?
+  end
 end
