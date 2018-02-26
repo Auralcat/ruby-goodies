f3ceab9c4518ffec9b7b6299c99d56ebdd9a5fe5
Auralcat <ashleyzt@hi2.in>
2018-02-25 23:27:10 -0300

    Create parser_int_proc

--- passgen/passgen.rb
+++ passgen/passgen.rb
@@ -26,33 +26,45 @@ class PasswordGenerator
       # For now I'm using emojify
 
       # We need to put a space between the aliases for emojify to work
-      `emojify #{EMOJI.sample(@options[:length]).join(" ")}`.delete(" ")
+      `emojify #{EMOJI.sample(@options["length"]).join(" ")}`.delete(" ")
     elsif @options["alphanumeric"]
-      ALPHANUMERIC.sample(@options[:length]).join
+      ALPHANUMERIC.sample(@options["length"]).join
     else
-      ALL_CHARS.sample(@options[:length]).join
+      ALL_CHARS.sample(@options["length"]).join
     end
   end
 
 end
 
 # Treating options on input, initialize base values
-options = {:length => 10,   :count => 1}
+options = {"length" => 10, "count" => 1}
 
 OptionParser.new do |parser|
   parser.banner = "CLI password generator."
 
   # This is just so we don't have to type parser.on ALL the time
-  parser_proc = -> (short_option, long_option, description) {
-    parser.on(short_option, long_option, description) do |opt|
-      options[long_option.gsub("-", "")] = opt
+  parser_bool_proc = -> (short_opt, long_opt, description) {
+    parser.on(short_opt, long_opt, description) do |opt|
+      options[long_opt.gsub("-", "")] = opt
+    end
+  }
+
+  # Passing integers to the option
+  parser_int_proc = -> (short_opt, long_opt, description) {
+    parser.on(short_opt, long_opt, Integer, description) do |value|
+      options[long_opt.gsub("-", "")] = value
     end
   }
 
   # Working with the boolean options
-  parser_proc.call("-a", "--alphanumeric", "Generate password only with letters and numbers")
-  parser_proc.call("-c", "--count", "Generate COUNT passwords")
-  parser_proc.call("-e", "--emoji", "Generate emoji-only passwords")
+  parser_bool_proc.call("-a", "--alphanumeric",
+                        "Generate password only with letters and numbers")
+  parser_bool_proc.call("-e", "--emoji", "Generate emoji-only passwords")
+
+  # Options that take a number as argument
+  parser_int_proc.call("-c", "--count COUNT", "Generate COUNT passwords")
+  parser_int_proc.call("-l", "--length LENGTH",
+                       "Generate password with length LENGTH. Default is 10.")
 
   # This is a different case
   parser.on("-h",
@@ -60,19 +72,11 @@ OptionParser.new do |parser|
             "Display this help message.") do ||
     puts parser
   end
-
-  # We need to cast the length to Integer here
-  parser.on("-l",
-            "--length LENGTH",
-            Integer,
-            "Generate password with length LENGTH. Default is 10.") do |l|
-    options[:length] = l
-  end
 end.parse!
 
 # Tie everything together
 p = PasswordGenerator.new(options)
 
-options[:count].times do
+options["count"].times do
   puts p.generate
 end
