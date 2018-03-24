7eff44a94356af606dbeef6b480c017df3600613
Auralcat <ashleyzt@hi2.in>
2018-03-24 19:55:16 -0300

    Create selector object, adapt int options to particles

--- lorem/lorem.rb
+++ lorem/lorem.rb
@@ -6,17 +6,26 @@ require 'optparse'
 require 'faker'
 
 # Treating options on input, initialize base values
-generator_options = { lorem_ipsum: true }
 particle_options = { paragraphs: 4 }
+
+# Helper selector object
+class OptionSelector
+  def initialize(generators, particles)
+    @generators = generator_options
+    @particles = particle_options
+  end
+end
 
 # Generator object
 class LoremGenerator
-  def initialize(generator_options, particle_options)
-    @generator_options = generator_options
+  def initialize(particle_options)
     @particle_options = particle_options
   end
 
+  # You can only generate one of either paragraphs, phrases or words.
+  # If a conflict comes up, pick the shortest one.
   def generate
+
   end
 end
 
@@ -24,16 +33,16 @@ OptionParser.new do |parser|
   parser.banner = 'CLI Lorem Ipsum generator.'
 
   # This is just so we don't have to type parser.on ALL the time
-  parser_bool_proc = -> (short_opt, long_opt, description) {
+  parser_generator_proc = -> (short_opt, long_opt, description) {
     parser.on(short_opt, long_opt, description) do |opt|
       default_options[long_opt.delete('-').to_sym] = opt
     end
   }
 
   # Passing integers to the option
-  parser_int_proc = -> (short_opt, long_opt, description) {
+  parser_particle_proc = -> (short_opt, long_opt, description) {
     parser.on(short_opt, long_opt, Integer, description) do |value|
-      default_options[long_opt.gsub(/[A-Z-]+/, '').delete(' ').to_sym] = value
+      particle_options[long_opt.gsub(/[A-Z-]+/, '').delete(' ').to_sym] = value
     end
   }
 
@@ -43,6 +52,17 @@ OptionParser.new do |parser|
             'Display this help message.') do
     puts parser
   end
+
+  # Particle options
+  parser_particle_proc.call('-w',
+                       '--words',
+                       'Generate WORDS words.')
+  parser_particle_proc.call('-s',
+                       '--sentences',
+                       'Generate SENTENCES sentences.')
+  parser_particle_proc.call('-p',
+                       '--paragraphs',
+                       'Generate PARAGRAPHS paragraphs.')
 end.parse!
 
 # Execute when called from the command line.
