0a0a34b4411c126f74f724dce5f958360cc43639
Auralcat <ashleyzt@hi2.in>
2018-03-24 15:48:22 -0300

    Define paragraph regex, start word count tests

--- lorem/test_lorem.rb
+++ lorem/test_lorem.rb
@@ -13,10 +13,17 @@ class TestLoremGenerator < Minitest::Test
   end
 
   def test_output_returns_4_medium_paragraphs_on_default
+    # Split the paragraphs and count them
+    assert_equal(output.split("\n\n").length, 4)
   end
+
+  # Atomic paragraph testing
+  single_paragraph = LoremGenerator.new(:paragraphs => 1)
 
   # Paragraph checking
   def test_short_paragraphs_have_between_25_and_30_words
+    cond = single_paragraph.output.split(" ").length <= 30 and single_paragraph.output.split(" ") >= 25
+    assert_equal(single_paragraph.output.split(" ").length )
   end
 
   def test_medium_paragraphs_have_between_50_and_60_words
