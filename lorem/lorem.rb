49ea8f925e2304d96bc64b8f79b68f15e61e4b05
Auralcat <ashleyzt@hi2.in>
2018-03-24 19:45:46 -0300

    Split options between generator and particle opts

--- lorem/lorem.rb
+++ lorem/lorem.rb
@@ -6,52 +6,48 @@ require 'optparse'
 require 'faker'
 
 # Treating options on input, initialize base values
-default_options = {:paragraphs => 4, :lorem_ipsum => true}
+generator_options = { lorem_ipsum: true }
+particle_options = { paragraphs: 4 }
 
 # Generator object
 class LoremGenerator
-
-  # Word sets.
-  def initialize(options)
-    @options = options
+  def initialize(generator_options, particle_options)
+    @generator_options = generator_options
+    @particle_options = particle_options
   end
 
   def generate
-    if (@options[:lorem_ipsum])
-      Faker::Lorem.paragraphs(@options[:paragraphs])
-    end
   end
 end
 
 OptionParser.new do |parser|
-  parser.banner = "CLI Lorem Ipsum generator."
+  parser.banner = 'CLI Lorem Ipsum generator.'
 
   # This is just so we don't have to type parser.on ALL the time
   parser_bool_proc = -> (short_opt, long_opt, description) {
     parser.on(short_opt, long_opt, description) do |opt|
-      default_options[long_opt.gsub("-", "").to_sym] = opt
+      default_options[long_opt.delete('-').to_sym] = opt
     end
   }
 
   # Passing integers to the option
   parser_int_proc = -> (short_opt, long_opt, description) {
     parser.on(short_opt, long_opt, Integer, description) do |value|
-      default_options[long_opt.gsub(/[A-Z-]+/, "").delete(" ").to_sym] = value
+      default_options[long_opt.gsub(/[A-Z-]+/, '').delete(' ').to_sym] = value
     end
   }
 
   # Display help message
-  parser.on("-h",
-            "--help",
-            "Display this help message.") do ||
+  parser.on('-h',
+            '--help',
+            'Display this help message.') do
     puts parser
   end
-
 end.parse!
 
 # Execute when called from the command line.
 # Equivalent to Python's if __name__ == "main"
-if __FILE__ == $0
+if $PROGRAM_NAME == __FILE__
   p = Lorem.new(default_options)
   puts p.generate
 end
