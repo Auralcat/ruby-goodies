043c9784d1046f713005375b7b777fe3b6fc7cca
Auralcat <ashleyzt@hi2.in>
2018-03-24 18:46:09 -0300

    Finish writing tests

--- lorem/test_lorem.rb
+++ lorem/test_lorem.rb
@@ -1,36 +1,50 @@
 require 'minitest/autorun'
 require './passgen'
 
+# Helper functions
+class String
+  def count_words
+    self.split(" ").length
+  end
+
+  def word_count_in_range?(start, stop)
+    self.word_count <= stop and self.word_count >= start
+  end
+end
+
+# Test class
 class TestLoremGenerator < Minitest::Test
 
   def setup
     @lorem = LoremGenerator.new()
+    @single_paragraph = LoremGenerator.new(:paragraphs => 1)
   end
 
   # Testing default output
   def test_output_starts_with_lorem_ipsum_on_default
-    assert_equal(/^Lorem ipsum/.match(@lorem), true)
+    assert_equal(/^Lorem ipsum/.match(@lorem.output), true)
   end
 
   def test_output_returns_4_medium_paragraphs_on_default
     # Split the paragraphs and count them
-    assert_equal(output.split("\n\n").length, 4)
+    assert_equal(@lorem.output.split("\n\n").length, 4)
   end
 
-  # Atomic paragraph testing
-  single_paragraph = LoremGenerator.new(:paragraphs => 1)
+  def test_output_returns_a_string
+    assert_equal(@lorem.output.is_a?(String), true)
+  end
 
   # Paragraph checking
   def test_short_paragraphs_have_between_25_and_30_words
-    cond = single_paragraph.output.split(" ").length <= 30 and single_paragraph.output.split(" ") >= 25
-    assert_equal(single_paragraph.output.split(" ").length )
+    assert_equal(@single_paragraph.output.word_count_in_range?(25, 30), true)
   end
 
   def test_medium_paragraphs_have_between_50_and_60_words
+    assert_equal(@single_paragraph.output.word_count_in_range?(50, 60), true)
   end
 
   def test_long_paragraphs_have_between_75_and_90_words
+    assert_equal(@single_paragraph.output.word_count_in_range?(75, 90), true)
   end
-
 
 end
