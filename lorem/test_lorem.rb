85e4477472c5f6a3437e1184fa3765f0ce3ec7cc
Auralcat <ashleyzt@hi2.in>
2018-03-24 15:26:13 -0300

    Touch tests for lorem.rb

--- lorem/test_lorem.rb
+++ lorem/test_lorem.rb
@@ -4,11 +4,26 @@ require './passgen'
 class TestLoremGenerator < Minitest::Test
 
   def setup
-
+    @lorem = LoremGenerator.new()
   end
 
-  # def test_password_length_is_10_on_default
-  #   assert_equal 10, @passgen.generate.length
-  # end
+  # Testing default output
+  def test_output_starts_with_lorem_ipsum_on_default
+    assert_equal(/^Lorem ipsum/.match(@lorem), true)
+  end
+
+  def test_output_returns_4_medium_paragraphs_on_default
+  end
+
+  # Paragraph checking
+  def test_short_paragraphs_have_between_25_and_30_words
+  end
+
+  def test_medium_paragraphs_have_between_50_and_60_words
+  end
+
+  def test_long_paragraphs_have_between_75_and_90_words
+  end
+
 
 end
