433e45eb1cb82021edf8ab58306d4f623583eece
Auralcat <ashleyzt@hi2.in>
2018-04-07 16:44:39 -0300

    Touch files for CSS primer

---
+++ css-primer/css_primer.rb
@@ -0,0 +1,2 @@
+# The idea here is to generate a CSS file from a HTML file.
+# We'll use Nokogiri to do the parsing
---
+++ css-primer/test_css_primer.rb
@@ -0,0 +1,10 @@
+require 'minitest/autorun'
+require './lorem'
+require 'faker'
+
+# Helper functions
+
+# Test class
+class TestLoremGenerator < Minitest::Test
+
+end
