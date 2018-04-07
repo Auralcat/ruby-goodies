6d32873da78e3f99854c7446ce179c97ab8e408b
Auralcat <ashleyzt@hi2.in>
2018-04-07 17:34:15 -0300

    Finished scraping classes from HTML file

--- css-primer/css_primer.rb
+++ css-primer/css_primer.rb
@@ -7,18 +7,25 @@ require 'optparse'
 # Get this through optparse
 file_path = ""
 # Open file using Nokogiri
-html_file = Nokogiri::HTML.open(file_path)
+html_file = Nokogiri::HTML(open(Dir.home + "/parse_this.html"))
 
 # Extra step: ensure HTML is well formed
 
 # Put all found classes in here
 html_classes = []
 html_file.css('*').each do |element|
-  buf = element.attr('class').value
-  if buf.include?(" ")
-    buf.split(' ').each do |item|
-      html_classes << item
+  buf = element.attr('class')
+  if !buf.nil?
+    if buf.include?(" ")
+      puts "buf has more than one word: #{buf}"
+      buf.split(' ').each do |item|
+        html_classes << item
+      end
+    else
+      html_classes << buf
     end
   end
-  html_classes << buf if buf != nil
 end
+
+puts html_classes.uniq
+puts "#{html_classes.uniq.length} classes have been scraped."
