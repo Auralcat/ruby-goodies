f30ba19723d00e05a85d1e1f4ea418a155776e27
Auralcat <ashleyzt@hi2.in>
2018-04-07 17:18:26 -0300

    Get classes from HTML file

--- css-primer/css_primer.rb
+++ css-primer/css_primer.rb
@@ -1,2 +1,24 @@
 # The idea here is to generate a CSS file from a HTML file.
 # We'll use Nokogiri to do the parsing
+
+require 'nokogiri'
+require 'optparse'
+
+# Get this through optparse
+file_path = ""
+# Open file using Nokogiri
+html_file = Nokogiri::HTML.open(file_path)
+
+# Extra step: ensure HTML is well formed
+
+# Put all found classes in here
+html_classes = []
+html_file.css('*').each do |element|
+  buf = element.attr('class').value
+  if buf.include?(" ")
+    buf.split(' ').each do |item|
+      html_classes << item
+    end
+  end
+  html_classes << buf if buf != nil
+end
