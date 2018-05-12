abfca058c09ced20b9640b8810b5cce7fe555b50
Auralcat <ashleyzt@hi2.in>
2018-05-12 15:13:09 -0300

    Start writing tests for get_html_source

--- css-primer/test_css_primer.rb
+++ css-primer/test_css_primer.rb
@@ -1,10 +1,10 @@
 require 'minitest/autorun'
-require './lorem'
-require 'faker'
 
 # Helper functions
 
 # Test class
 class TestLoremGenerator < Minitest::Test
-
+  def test_get_html_source_returns_a_string
+    assert_equal get_html_source().class, String
+  end
 end
